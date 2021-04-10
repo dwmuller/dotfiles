0ba68ef785beb5e8cfa2d7e22d6d0566d9dde53a
Dan Muller <dan.muller@dmuller.us>
2021-04-10 10:09:48 -0400

    Added some support for bash completions.

---
+++ bash/completions.bash
@@ -0,0 +1,13 @@
+
+# Source bash completions if we can find them.
+if ! shopt -oq posix; then
+  if [ -f /usr/share/bash-completion/bash_completion ]; then
+    source /usr/share/bash-completion/bash_completion
+  elif [ -f /etc/bash_completion ]; then
+    source /etc/bash_completion
+  fi
+fi
+
+# Set up terraform completions if we have terraform installed.
+command -v terraform &>/dev/null && complete -C terraform terraform
+
