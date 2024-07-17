f5e2d4d32e536dff8f8a5d5506f540db3686188c
Dan Muller <dan.muller@dmuller.us>
2024-07-17 16:20:48 -0400

    Add element to PowerShell path.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,1 +1,4 @@
+$binpath = "$($env:HOMEDRIVE)$($env:HOMEPATH)\.dotfiles\bin\win"
+if (! $env:Path.StartsWith($binpath)) {$env:Path = $binpath + ';' + $env:Path}
+
 oh-my-posh --init --shell pwsh --config ~/.config.omp.json | Invoke-Expression
