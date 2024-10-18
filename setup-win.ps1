21476f3ef88857c083c1c47317ab53ccd4f7b577
Dan Muller <dan.muller@dmuller.us>
2024-10-18 09:27:26 -0400

    Shifting towards winget and non-admin installs.

--- setup-win.ps1
+++ setup-win.ps1
@@ -11,37 +11,45 @@
     $Host.UI.RawUI.WindowTitle = $myInvocation.MyCommand.Definition + "(Elevated)"
     clear-host
  } else {
-    # We are not running "as Administrator" - so relaunch as administrator
+    Write-Host "Running as non-admin."
+    # # We are not running "as Administrator" - so relaunch as administrator
     
-    # Create a new process object that starts PowerShell
-    $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
+    # # Create a new process object that starts PowerShell
+    # $newProcess = new-object System.Diagnostics.ProcessStartInfo "PowerShell";
     
-    # Specify the current script path and name as a parameter
-    $newProcess.Arguments = $myInvocation.MyCommand.Definition;
+    # # Specify the current script path and name as a parameter
+    # $newProcess.Arguments = $myInvocation.MyCommand.Definition;
     
-    # Indicate that the process should be elevated
-    $newProcess.Verb = "runas";
+    # # Indicate that the process should be elevated
+    # $newProcess.Verb = "runas";
     
-    # Start the new process
-    [System.Diagnostics.Process]::Start($newProcess);
+    # # Start the new process
+    # [System.Diagnostics.Process]::Start($newProcess);
     
-    # Exit from the current, unelevated, process
-    exit
+    # # Exit from the current, unelevated, process
+    # exit
 }  
 
 function Install-PackagesUsingWinget {
+    Param([string[]] $pkgs)
     # winget downloads and installs even if a package is already installed,
     # so check first. Also, winget can only install one program per invocation.
-    foreach ($pkg in $args) {
+    foreach ($pkg in $pkgs) {
         winget list --accept-source-agreements --exact -q $pkg | Out-Null
         if ($?) {
             Write-Host "Already installed (winget): $pkg"
         } else {
             Write-Host "Installing (winget): $pkg"
-            winget install --exact --silent  --accept-package-agreements $pkg 
+            winget install --exact --silent  --accept-package-agreements $pkg --scope user
         }
     }
 }
+
+# We use winget by preference.
+Install-PackagesUsingWinget Git.Git, gerardog.gsudo, JanDeDobbeleer.OhMyPosh,
+    Microsoft.PowerShell, Microsoft.WindowsTerminal, albertony.npiperelay,
+    Python.Python.3.13,
+    Microsoft.VisualStudioCode
 
 # Install chocolatey.
 Set-ExecutionPolicy Bypass -Scope Process -Force;
@@ -56,7 +64,5 @@ if (-not (Get-Command "choco.exe" -ErrorAction SilentlyContinue)) {
 # choco-packages.config, along with any installation parameters.
 $chocoConfig = Join-Path $PSScriptRoot "choco-packages.config" -Resolve -ErrorAction Inquire
 (choco install -y --limitoutput $chocoConfig)
-
-Install-PackagesUsingWinget JanDeDobbeleer.OhMyPosh Microsoft.PowerShell Microsoft.WindowsTerminal Microsoft.OneDrive
 
 Read-Host "Press ENTER to continue"
