bc635a7324e031a3e5f183ff050d13be148ab1f0
Dan Muller <dan.muller@dmuller.us>
2024-10-18 08:41:02 -0400

    Cope with variances in PS profile file location.

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,5 @@
-$binpath = "$($env:HOMEDRIVE)$($env:HOMEPATH)\.dotfiles\bin\win"
+$binroot = "$env:HOMEDRIVE$env:HOMEPATH"
+$binpath = "$binroot\bin;$binroot\.dotfiles\bin\win"
 if (! $env:Path.StartsWith($binpath)) {$env:Path = $binpath + ';' + $env:Path}
 
 oh-my-posh --init --shell pwsh --config ~/.config.omp.json | Invoke-Expression
