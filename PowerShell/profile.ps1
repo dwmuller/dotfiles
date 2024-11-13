58747c750d327e048aba07afc47b84ccb81295cd
Dan Muller <dan.muller@dmuller.us>
2024-11-13 11:22:40 -0500

    Improvements to path mgmt in PS profile.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,5 +1,17 @@
-$binroot = "$env:HOMEDRIVE$env:HOMEPATH"
-$binpath = "$binroot\bin;$binroot\.dotfiles\bin\win"
-if (! $env:Path.StartsWith($binpath)) {$env:Path = $binpath + ';' + $env:Path}
+
+function Remove-Path([string]$path) {
+    $env:Path = (($env:Path -split ';') -ne $path) -join ';'
+}
+function Add-PathFirst([string]$path) {
+    Remove-Path $path
+    if (Test-Path $path) {$env:Path = "$path;$env:Path"}
+}
+function Add-PathLast([string]$path) {
+    Remove-Path $path
+    if (Test-Path $path) {$env:Path = "$env:Path;$path"}
+}
+
+Add-PathFirst "$HOME\.dotfiles\bin\win"
+Add-PathFirst "$HOME\bin"
 
 oh-my-posh --init --shell pwsh --config ~/.config.omp.json | Invoke-Expression
