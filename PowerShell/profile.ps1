07dcaad6c38a3ac06995486229bf4a44deb8490f
Dan Muller <dan.muller@dmuller.us>
2025-03-06 15:28:04 -0500

    Added some conveniences to PS profile.

--- PowerShell/profile.ps1
+++ PowerShell/profile.ps1
@@ -1,4 +1,5 @@
 
+
 function Remove-Path([string]$path) {
     $env:Path = (($env:Path -split ';') -ne $path) -join ';'
 }
@@ -15,3 +16,15 @@ Add-PathFirst "$HOME\.dotfiles\bin\win"
 Add-PathFirst "$HOME\bin"
 
 oh-my-posh --init --shell pwsh --config ~/.config.omp.json | Invoke-Expression
+
+# Convenient online help invocation, since PS lacks a text-based pager.
+function Get-HelpOnline {Get-Help -Online @args}
+New-Alias -Name ohelp -Value Get-HelpOnline
+
+# Or maybe we can haz pager ...
+&{
+    $local:P="$env:LOCALAPPDATA\Programs\Git\usr\bin\less.exe"
+    if (Test-Path -Path $local:P) {
+        $Env:PAGER=$local:P
+    }
+}
